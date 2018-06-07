65693f040b62fb2faac9ecb6cc33276d2b81e72b
aggre <hiroyuki.aggre@gmail.com>
2018-06-08 00:21:17 +0900

    Add test case: When the third argument is provided, its value is rendered as initial content

--- src/directive/subscribe.test.ts
+++ src/directive/subscribe.test.ts
@@ -42,20 +42,12 @@ describe('subscribe directive', () => {
 		)
 		let count = 0
 		render(
-			await html`${subscribe(
-				timer,
-				x => {
-					count += 1
-					return html`<p>${x}</p>`
-				},
-				html`<p>placeholder</p>`
-			)}`,
+			await html`${subscribe(timer, x => {
+				count += 1
+				return html`<p>${x}</p>`
+			})}`,
 			document.body
 		)
-		await sleep(0)
-		expect(
-			(document.body.querySelector('p') as HTMLParagraphElement).innerText
-		).to.be('placeholder')
 		await sleep(100)
 		const p = slotSelector(
 			document.body.querySelector('f-e-subscribe'),
@@ -64,5 +56,31 @@ describe('subscribe directive', () => {
 		)
 		expect((p as HTMLParagraphElement).innerText).to.be('10')
 		expect(count).to.be(10)
+	})
+
+	it('When the third argument is provided, its value is rendered as initial content', async () => {
+		const timer = _timer(10, 1).pipe(
+			filter(x => x > 0),
+			take(1)
+		)
+		render(
+			await html`${subscribe(
+				timer,
+				x => html`<p>${x}</p>`,
+				html`<p>placeholder</p>`
+			)}`,
+			document.body
+		)
+		await sleep(0)
+		expect(
+			(document.body.querySelector('p') as HTMLParagraphElement).innerText
+		).to.be('placeholder')
+		await sleep(11)
+		const p = slotSelector(
+			document.body.querySelector('f-e-subscribe'),
+			'slot',
+			'p'
+		)
+		expect((p as HTMLParagraphElement).innerText).to.be('1')
 	})
 })
