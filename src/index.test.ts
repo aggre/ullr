fa246e8637ca6ce0232b890770b349b12b6cdd24
aggre <hiroyuki.aggre@gmail.com>
2018-06-09 17:54:33 +0900

    Component are synchronous rendering

--- src/index.test.ts
+++ src/index.test.ts
@@ -32,10 +32,16 @@ describe('Rendering', () => {
 	})
 
 	describe('Rendering component', () => {
+		it('Component are synchronous rendering', () => {
+			const app = (content: string) => component(html`<main>${content}</main>`)
+			render(app('App'), document.body)
+			const shadow = document.body.querySelector('ullr-shdw')
+			expect(shadow).to.be.ok()
+		})
+
 		it('Render to the ShadowRoot in "ullr-shdw" element', async () => {
-			const app = async (content: string) =>
-				component(html`<main>${content}</main>`)
-			render(await app('App'), document.body)
+			const app = (content: string) => component(html`<main>${content}</main>`)
+			render(app('App'), document.body)
 			const shadow = document.body.querySelector('ullr-shdw')
 			await sleep(0)
 			const main = ((shadow as Element).shadowRoot as ShadowRoot).querySelector(
--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ const templates: Map<
 export const html = async (strings: TemplateStringsArray, ...values: any[]) =>
 	Promise.resolve(_html(strings, ...values))
 
-export const component = async (
+export const component = (
 	template: Promise<TemplateResult> | TemplateResult
 ) => {
 	const token = random()
 	templates.set(token, template)
-	return html`<ullr-shdw t$='${token}'></ullr-shdw>`
+	return _html`<ullr-shdw t$='${token}'></ullr-shdw>`
 }
 
 window.customElements.define(
