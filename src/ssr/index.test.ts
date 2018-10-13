94e60f7e847253a78e27c1b806ea5669eacadcc3
aggre <hiroyuki.aggre@gmail.com>
2018-10-14 01:02:50 +0900

    Add testing case

--- src/ssr/index.test.ts
+++ src/ssr/index.test.ts
@@ -7,4 +7,14 @@ describe('SSR', () => {
 		const result = await ssr(html`<div>Test</div>`, () => true)
 		strictEqual(result, '<div>Test</div>')
 	})
+
+	it('Rendering a async template', async () => {
+		const result = await ssr(
+			html`<div>${new Promise(resolve => {
+				setTimeout(() => resolve('Test'), 500)
+			})}</div>`,
+			h => h === '<div>Test</div>'
+		)
+		strictEqual(result, '<div>Test</div>')
+	})
 })
