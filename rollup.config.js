82293b623c7224989424f014715dac4dbe6a457c
aggre <hiroyuki.aggre@gmail.com>
2021-02-06 23:59:25 +0900

    change export path

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,42 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import typescript from 'rollup-plugin-typescript2'
-import multiEntry from 'rollup-plugin-multi-entry'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import multiEntry from '@rollup/plugin-multi-entry'
+import dts from 'rollup-plugin-dts'
 
-export const plugins = [
-	typescript(),
-	commonjs({
-		include: 'node_modules/**'
-	}),
-	resolve(),
-	multiEntry()
-]
-
-export default {
-	input: ['src/**/*.test.ts'],
-	output: {
-		file: 'dist/test.js',
-		format: 'umd'
+export default [
+	{
+		input: ['src/**/*.test.ts'],
+		output: {
+			file: 'dist/test.js',
+			format: 'umd'
+		},
+		plugins: [
+			typescript(),
+			commonjs({
+				include: 'node_modules/**'
+			}),
+			resolve(),
+			multiEntry()
+		]
 	},
-	plugins
-}
+	{
+		input: ['src/**/*.ts', '!**/*.test.ts'],
+		output: [
+			{
+				file: 'dist/index.mjs',
+				format: 'es',
+			},
+			{
+				file: 'dist/index.js',
+				format: 'cjs',
+			},
+		],
+		plugins: [multiEntry(), typescript()],
+	},
+	{
+		input: ['dist/**/*.d.ts', '!**/*.test.d.ts', '!**/d/*'],
+		output: [{ file: 'dist/ullr.d.ts', format: 'es' }],
+		plugins: [multiEntry(), dts()],
+	},
+]
